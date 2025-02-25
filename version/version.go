191e7f65d1d04c6c66153a5da28e0828e078ebce
jiajian.chi <jiajian.chi@zstack.io>
2025-02-25 10:11:46 +0800

    ci: change version arguments

--- version/version.go
+++ version/version.go
@@ -5,6 +5,6 @@ import "github.com/hashicorp/packer-plugin-sdk/version"
 var (
 	Version           = "1.0.0"
 	VersionPrerelease = "dev"
-	//VersionMetadata   = ""
-	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease)//, VersionMetadata)
+	VersionMetadata   = ""
+	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
